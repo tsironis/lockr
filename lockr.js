c1f275e291f1142d271e639a1737781fcc67f8f6
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2013-12-21 18:46:23 +0200

    Removes stuff from scaffolding the library

--- lockr.js
+++ lockr.js
@@ -1,14 +1,11 @@
 (function(root, factory) {
 
-  // Set up Backbone appropriately for the environment. Start with AMD.
   if (typeof define === 'function' && define.amd) {
     define(['exports'], function(exports) {
-      // Export global even in AMD case in case this script is loaded with
-      // others that may still expect a global Backbone.
-      root.Backbone = factory(root, exports);
+      root.Lockr = factory(root, exports);
     });
   } else {
-    root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
+    root.Lockr = factory(root, {});
   }
 
 }(this, function(root, Lockr) {
